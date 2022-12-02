55d2d5ad374394d1b8507671a5c36e1c299fcad0
josephsdavid <josephsd@smu.edu>
2022-12-02 00:02:04 -0600

    sum do, sum do not

--- src/day_2.jl
+++ src/day_2.jl
@@ -5,14 +5,14 @@ const input = readlines("data/day_2.txt")
 
 const t1 = split("A Y\nB X\nC Z", "\n")
 
-const score_dict = Dict(:rock => 1, :paper => 2, :scissors => 3)
+const points_dict = Dict(:rock => 1, :paper => 2, :scissors => 3)
 const win_conditions = Dict(:rock => :scissors, :paper => :rock, :scissors => :paper)
+
 const their_map = Dict("A" => :rock, "B" => :paper, "C" => :scissors)
 const our_map = Dict("X" => :rock, "Y" => :paper, "Z" => :scissors)
-const moves = (:rock, :paper, :scissors)
 
 function get_score(ours::Symbol, theirs::Symbol)
-    score = score_dict[ours]
+    score = points_dict[ours]
     theirs == ours && return score + 3
     win_condition = win_conditions[ours]
     if theirs == win_condition
@@ -28,13 +28,10 @@ end
 
 
 function part_1(input)
-    score = 0
-    foreach(input) do row
+    return sum(input) do row
         theirs, ours = split(row, " ")
-        round_score = get_score(ours, theirs)
-        score += round_score
+        return get_score(ours, theirs)
     end
-    return score
 end
 
 @info part_1(t1)
@@ -42,27 +39,23 @@ end
 
 const strategies = Dict("X" => :lose, "Y" => :draw, "Z" => :win)
 
-function strategize(outcome::Symbol)
+function execute_evil_plan(outcome::Symbol, theirs::Symbol)
     if outcome == :draw
-        return identity
+        return theirs
     elseif outcome == :win
-        return (theirs) -> Dict(values(win_conditions), keys(win_conditions))[theirs] # they are the loser
+        return Dict(zip(values(win_conditions), keys(win_conditions)))[theirs] # they are the loser
     else
-        return (theirs) -> win_conditions[theirs] # they are the winner
+        return win_conditions[theirs] # they are the winner
     end
 end
 
 function part_2(input)
-    score = 0
-    foreach(input) do row
-        theirs, plan = split(row, " ")
+    return sum(input) do row
+        theirs, outcome = split(row, " ")
         theirs = their_map[theirs]
-        ours = strategize(strategies[plan])(theirs)
-        round_score = get_score(ours, theirs)
-        score += round_score
+        ours = execute_evil_plan(strategies[outcome], theirs)
+        return get_score(ours, theirs)
     end
-    return score
 end
-
 @info part_2(t1)
 @info part_2(input)
