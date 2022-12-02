2ac9ae1cc19ddffa7b2b81c9ef0f574d2e263792
josephsdavid <josephsd@smu.edu>
2022-12-01 22:33:24 -0600

    initial commit

---
+++ src/day_1.jl
@@ -0,0 +1,16 @@
+# https://adventofcode.com/2022/day/1
+using AdventOfCode
+
+input = read("2022/data/day_1.txt")
+
+parse_input(s) = parse.(Int64, filter(!isempty, split.(split(s, "\n\n"))))
+
+function part_1(input)
+    return maximum(sum, parse_input(input))
+end
+@info part_1(input)
+
+function part_2(input)
+    return (sort ∘ sum ∘ x -> last(x, 3))(sum.(parse_input(input)))
+end
+@info part_2(input)
---
+++ src/day_2.jl
@@ -0,0 +1,14 @@
+# https://adventofcode.com/2022/day/2
+using AdventOfCode
+
+input = readlines("2022/data/day_2.txt")
+
+function part_1(input)
+    nothing
+end
+@info part_1(input)
+
+function part_2(input)
+    nothing
+end
+@info part_2(input)
