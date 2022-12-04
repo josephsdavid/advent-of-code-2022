21ee36156845cff562425e89e6dba97e959ccb8a
josephsdavid <josephsd@smu.edu>
2022-12-03 23:40:35 -0600

    remove a collect

--- src/day_4.jl
+++ src/day_4.jl
@@ -1,5 +1,6 @@
 # https://adventofcode.com/2022/day/4
 using AdventOfCode
+using BenchmarkTools
 
 const t1 = split("""2-4,6-8
 2-3,4-5
@@ -12,21 +13,21 @@ input = readlines("data/day_4.txt")
 
 function part_1(input)
     return sum(input) do line
-        groups = (Set ∘ collect ∘ eval ∘ Meta.parse).(replace.(split(line, ","), "-"=>":"))
+        groups = (Set ∘ eval ∘ Meta.parse).(replace.(split(line, ","), "-"=>":"))
         return length(union(groups...)) == maximum(length.(groups))
     end
 end
 
 part_1(t1)
 
-@info part_1(input)
+@btime part_1(input)
 
 function part_2(input)
     return sum(input) do line
-        groups = (Set ∘ collect ∘ eval ∘ Meta.parse).(replace.(split(line, ","), "-"=>":"))
+        groups = (Set ∘ eval ∘ Meta.parse).(replace.(split(line, ","), "-"=>":"))
         return length(intersect(groups...)) > 0
     end
 end
-@info part_2(t1)
+@btime part_2(t1)
 
-@info part_2(input)
+@btime part_2(input)
