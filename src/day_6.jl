7d6ac3f77b4dcd809b9f35bd4190ccd6bd5f2a00
josephsdavid <josephsd@smu.edu>
2022-12-06 00:08:39 -0600

    typo

--- src/day_6.jl
+++ src/day_6.jl
@@ -15,7 +15,7 @@ part_2(input) = marker(input, 14)
 
 
 function solve_fast(z, w, step = 1)
-    @simd for i in 1:step:length(z)-2+1
+    @simd for i in 1:step:length(z)-w+1
         x = @inbounds view(z,i:i+w-1)
         if length(unique(x)) == w
             return i + w -1
@@ -26,24 +26,36 @@ end
 p1_fast(x) = solve_fast(x, 4)
 p2_fast(x) = solve_fast(x, 14)
 
-@testset "day 6" begin
-    test_p1(inp, val) = @test part_1(inp) == val == p1_fast(inp)
-    @testset "part 1" begin
-        test_p1(t1, 7)
-        test_p1("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)
-        test_p1("nppdvjthqldpwncqszvftbrmjlhg", 6)
-        test_p1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)
-        test_p1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)
-    end
-    test_p2(inp, val) = @test part_2(inp) == val == p2_fast(inp)
-    @testset "part 2" begin
-        test_p2(t1, 19)
-        test_p2("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)
-        test_p2("nppdvjthqldpwncqszvftbrmjlhg", 23)
-        test_p2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)
-        test_p2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)
-    end
+####
+#### tests
+####
+
+test_p1(inp, val) = @test part_1(inp) == val == p1_fast(inp)
+
+# precompile before testing so the tests are fast
+test_p1("abcd", 4)
+
+@testset "part 1" begin
+    test_p1(t1, 7)
+    test_p1("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)
+    test_p1("nppdvjthqldpwncqszvftbrmjlhg", 6)
+    test_p1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)
+    test_p1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)
 end
+
+test_p2(inp, val) = @test part_2(inp) == val == p2_fast(inp)
+
+@testset "part 2" begin
+    test_p2(t1, 19)
+    test_p2("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)
+    test_p2("nppdvjthqldpwncqszvftbrmjlhg", 23)
+    test_p2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)
+    test_p2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)
+end
+
+####
+#### run
+####
 
 @info "part 1, no optimization"
 @info @btime part_1(input)
